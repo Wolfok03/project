4a29d0cc209aa27302c4560f10b4f1950491d5e4
Hung <nguyenhung2102004@gmail.com>
2024-08-30 12:47:29 +0700

    hybyhbubhub

---
+++ src/main/java/com/example/pro2/Model/chitiethoadon.java
@@ -0,0 +1,71 @@
+package com.example.pro2.Model;
+
+import java.util.Date;
+
+public class chitiethoadon {private String thongtinsanpham;
+    private Date ngaythanhtoan;
+    private int masanpham;
+    private int giatien;
+    private int soluong;
+    private String thongtinkhachhang;
+    private String phuongthucthanhtoan;
+
+
+    public String getthongtinsanpham() {
+        return thongtinsanpham;
+    }
+
+    public void setthongtinsanpham(String thongtinsanpham) {
+        this.thongtinsanpham = thongtinsanpham;
+    }
+
+    public int getMasanpham() {
+        return masanpham;
+    }
+
+    public void setMasanpham(int masanpham) {
+        this.masanpham = masanpham;
+    }
+
+    public int getGiatien() {
+        return giatien;
+    }
+
+    public void setGiatien(int giatien) {
+        this.giatien = giatien;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+
+    public String getThongtinkhachhang() {
+        return thongtinkhachhang;
+    }
+
+    public void setThongtinkhachhang(String thongtinkhachhang) {
+        this.thongtinkhachhang = thongtinkhachhang;
+    }
+
+    public Date getngaythanhtoan() {
+        return ngaythanhtoan;
+    }
+
+    public void setngaythanhtoan(Date ngaythanhtoan) {
+        this.ngaythanhtoan = ngaythanhtoan;
+    }
+
+
+    public String getphuongthucthanhtoan() {
+        return phuongthucthanhtoan;
+    }
+
+    public void setphuongthucthanhtoan(String phuongthucthanhtoan) {
+        this.phuongthucthanhtoan = phuongthucthanhtoan;
+    }
+
+}
---
+++ src/main/java/com/example/pro2/Model/phuonthucthanhtoan.java
@@ -0,0 +1,39 @@
+package com.example.pro2.Model;
+
+public class phuonthucthanhtoan { private String tructiep;
+    private String trunggian;
+    private String thongtin;
+    private int makhuyenmai;
+
+    public String getTructiep() {
+        return tructiep;
+    }
+
+    public void setTructiep(String tructiep) {
+        this.tructiep = tructiep;
+    }
+
+    public String getTrunggian() {
+        return trunggian;
+    }
+
+    public void setTrunggian(String trunggian) {
+        this.trunggian = trunggian;
+    }
+
+    public String getThongtin() {
+        return thongtin;
+    }
+
+    public void setThongtin(String thongtin) {
+        this.thongtin = thongtin;
+    }
+
+    public int getMakhuyenmai() {
+        return makhuyenmai;
+    }
+
+    public void setMakhuyenmai(int makhuyenmai) {
+        this.makhuyenmai = makhuyenmai;
+    }
+}
