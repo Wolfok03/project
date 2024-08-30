2230b5d137ce025034b9597ca943b55a61a2c14c
demo <dc12072k3@gmail.com>
2024-08-30 11:04:01 +0700

    update

--- src/main/java/com/example/pro2/services/ProductsRespository.java
+++ src/main/java/com/example/pro2/services/ProductsRespository.java
@@ -1,6 +1,6 @@
 package com.example.pro2.services;
 
-import com.example.pro2.Model.product;
+import com.example.pro2.models.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductsRespository extends JpaRepository<product, Integer> {
